819060937e500fe9094ea122397e0e78a8bdd99c
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2023-04-12 11:04:09 +0800

    fix

--- beancount_periodic/common/number.py
+++ beancount_periodic/common/number.py
@@ -1,16 +1,20 @@
-from decimal import Decimal
+from decimal import Decimal, DecimalException
 import math
 
 
 def smart_place_num(ref: Decimal, div: Decimal):
     round_place_added = int(round(math.log10(abs(div)), 0)) + 1 if div != 0 else 0
-    return max(- ref.as_tuple().exponent + round_place_added, 2)
+    return min(max(- ref.as_tuple().exponent + round_place_added, 2), 6)
 
 
 def round_and_remainder(target: Decimal, place_num):
-    round_value = round(target, place_num)
-    remainder = target - round_value
-    return round_value, remainder
+    try:
+      round_value = round(target, place_num)
+      remainder = target - round_value
+      return round_value, remainder
+    except DecimalException as e:
+      print(e)
+      return target, 0
 
 
 def remove_exponent_zero(num: Decimal):
