b4ef4f6d91be6a2049bb04e2d337bbefd074f2fb
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2023-04-12 11:08:50 +0800

    0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 with open('README.md', 'r', encoding='UTF-8') as f:
     LONG_DESCRIPTION = f.read()
